98e752af979732f39dd758568c7deba88d385e57
Darkmuzishn <melturk@hotmail.com>
2023-12-28 21:15:48 +0200

    Cleaned Up Game Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,11 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing="10px"
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardCont key={skeleton}>
